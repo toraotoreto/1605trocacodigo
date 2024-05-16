75671874394122240aead0a950f902d1b9b235d5
toraotoreto <111095126+toraotoreto@users.noreply.github.com>
2024-05-16 19:58:38 -0300

    Add files via upload

--- Calculator.java
+++ Calculator.java
@@ -1,17 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
+import java.util.Scanner;
 
-package com.mycompany.troca;
-
-/**
- *
- * @author 822154467
- */
-public class App {
-
+public class Calculator {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Selecione uma opção:");
+        System.out.println("1. Adição");
+        System.out.println("2. Subtração");
+        System.out.println("3. Multiplicação");
+        System.out.println("4. Divisão");
+
+        int option = scanner.nextInt();
+
+        System.out.println("Informe o primeiro número:");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Informe o segundo número:");
+        double num2 = scanner.nextDouble();
+
+        double result = 0;
+
+        switch (option) {
+            case 1:
+                result = num1 + num2;
+                break;
+            case 2:
+                result = num1 - num2;
+                break;
+            case 3:
+                result = num1 * num2;
+                break;
+            case 4:
+                if (num2!= 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Erro: divisão por zero!");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Opção inválida!");
+                return;
+        }
+
+        System.out.println("Resultado: " + result);
     }
 }
